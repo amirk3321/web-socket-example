e129495a78ae868ba74ddfee3ad905035cda0d52
amirk3321 <amirk3321@gmail.com>
2023-11-13 19:28:23 +0500

    Initial Commit

---
+++ WebSocketExample/WebSocketExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketExampleApp.swift
+//  WebSocketExample
+//
+//  Created by MA on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
